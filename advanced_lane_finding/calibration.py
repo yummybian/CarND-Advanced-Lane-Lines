bcfda62bf66c9b25377cdb20db5709ce349cd0f0
bin <bin@ngs.tech>
2017-08-28 19:14:55 +0800

    update

--- advanced_lane_finding/calibration.py
+++ advanced_lane_finding/calibration.py
@@ -5,14 +5,23 @@ import matplotlib.pyplot as plt
 
 
 class Calibration:
+    perspective_src = np.float32([[490, 482],[810, 482],
+                                  [1250, 720],[40, 720]])
+    perspective_dst = np.float32([[0, 0], [1280, 0], 
+                                  [1250, 720],[40, 720]])
+
+    
     def __init__(self, nx, ny):
-        # nx represents the number of inside corners in x
+        # nx represent the number of inside corners in x axis
+        # ny represent the number of inside corners in y axis
         self.nx, self.ny = nx, ny
         self.objpoints = [] # 3d points in real world space
         self.imgpoints = [] # 2d points in image plane
         self.cal_imgs = []
         self.cal_imgs_with_corners = []
         self.test_imgs = []
+        self.test_imgs_with_undistorted = []
+        self.test_imgs_with_birdseye = []
 
     def load_cal_imgs(self, cal_path):
         cal_img_names = glob.glob(cal_path)
@@ -26,8 +35,7 @@ class Calibration:
         objp = np.zeros((6*9,3), np.float32)
         objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
 
-        copy_imgs = [img.copy() for img in self.cal_imgs]
-        for img in copy_imgs:
+        for img in self.cal_imgs:
             # Convert to grayscale
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -36,22 +44,108 @@ class Calibration:
 
             # If found, draw corners
             if ret == True:
+                orig_img = img.copy()
                 self.objpoints.append(objp)
                 self.imgpoints.append(corners)
 
                 # Draw the corners
                 cv2.drawChessboardCorners(img, (self.nx, self.ny), corners, ret)
-                self.cal_imgs_with_corners.append(img)
-
-        print(len(self.cal_imgs))
-        print(len(self.cal_imgs_with_corners))
-
+                self.cal_imgs_with_corners.append(((orig_img, img)))
+   
+    def undistort(self):
+        for img in self.test_imgs:
+            img_size = (img.shape[1], img.shape[0])
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, img_size, None, None)
+            undist = cv2.undistort(img, mtx, dist, None, mtx)
+            self.test_imgs_with_undistorted.append((img, undist))
+            
+    def birds_eye(self):
+        for orig_img, undist_img in self.test_imgs_with_undistorted:
+            # Grab the image shape
+            img_size = (orig_img.shape[1], orig_img.shape[0])
+            # Given src and dst points, calculate the perspective transform matrix
+            M = cv2.getPerspectiveTransform(self.perspective_src, self.perspective_dst)
+            # Warp the image using OpenCV warpPerspective()
+            warped = cv2.warpPerspective(undist_img, M, img_size)
+            self.test_imgs_with_birdseye.append((orig_img, warped))
         
-    def show_plot(self):
-        for i, img in enumerate(self.cal_imgs):
+    def show_cal_plot(self):
+        for orig_img, img in self.cal_imgs_with_corners:
             f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8))
-            ax1.imshow(img)
+            ax1.imshow(orig_img)
             ax1.set_title('Original Image', fontsize=18)
-            ax2.imshow(self.cal_imgs_with_corners[i])
+            ax2.imshow(img)
             ax2.set_title('Image With Corners', fontsize=18)
+            
+    def show_test_plot(self):
+        for orig_img, img in self.test_imgs_with_undistorted:
+            f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8))
+            ax1.imshow(cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB))
+            ax1.set_title('Original Image', fontsize=18)
+            ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            ax2.set_title('Undistorted Image', fontsize=18)
+            
+    def show_birdseye_plot(self):
+        for orig_img, img in self.test_imgs_with_birdseye:
+            f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8))
+            ax1.imshow(cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB))
+            ax1.set_title('Original Image', fontsize=18)
+            ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            ax2.set_title('Warped Image', fontsize=18)
 
+    def show_threshold_plot(image, show=True):
+        img, M = birds_eye(image, display = False)
+
+        for orig_img, img in self.test_imgs_with_birdseye:
+
+            s_channel = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)[:,:,2]
+            
+            l_channel = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)[:,:,0]
+
+            b_channel = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)[:,:,2]   
+
+            # Threshold color channel
+            s_thresh_min = 180
+            s_thresh_max = 255
+            s_binary = np.zeros_like(s_channel)
+            s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+            
+            b_thresh_min = 155
+            b_thresh_max = 200
+            b_binary = np.zeros_like(b_channel)
+            b_binary[(b_channel >= b_thresh_min) & (b_channel <= b_thresh_max)] = 1
+            
+            l_thresh_min = 225
+            l_thresh_max = 255
+            l_binary = np.zeros_like(l_channel)
+            l_binary[(l_channel >= l_thresh_min) & (l_channel <= l_thresh_max)] = 1
+
+            #color_binary = np.dstack((u_binary, s_binary, l_binary))
+            
+            combined_binary = np.zeros_like(s_binary)
+            combined_binary[(l_binary == 1) | (b_binary == 1)] = 1
+
+            # Plotting thresholded images
+            f, ((ax1, ax2, ax3), (ax4,ax5, ax6)) = plt.subplots(2, 3, sharey='col', sharex='row', figsize=(10,4))
+            f.tight_layout()
+            
+            ax1.set_title('Original Image', fontsize=16)
+            ax1.imshow(orig_img, cv2.COLOR_BGR2RGB)
+            
+            ax2.set_title('Warped Image', fontsize=16)
+            ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype('uint8'))
+            
+            ax3.set_title('s binary threshold', fontsize=16)
+            ax3.imshow(s_binary, cmap='gray')
+            
+            ax4.set_title('b binary threshold', fontsize=16)
+            ax4.imshow(b_binary, cmap='gray')
+            
+            ax5.set_title('l binary threshold', fontsize=16)
+            ax5.imshow(l_binary, cmap='gray')
+
+            ax6.set_title('Combined color thresholds', fontsize=16)
+            ax6.imshow(combined_binary, cmap='gray')
+        
+        
+        
