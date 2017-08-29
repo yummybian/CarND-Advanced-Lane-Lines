e14886858a157080b752618533a92a1ec6ae2f31
bin <bin@ngs.tech>
2017-08-29 14:32:34 +0800

    update

--- advanced_lane_finding/calibration.py
+++ advanced_lane_finding/calibration.py
@@ -68,7 +68,114 @@ class Calibration:
             # Warp the image using OpenCV warpPerspective()
             warped = cv2.warpPerspective(undist_img, M, img_size)
             self.test_imgs_with_birdseye.append((orig_img, warped))
+
+
+    def combined_binary(self):
+        combined_binary_lst = []
+        for _, img in self.test_imgs_with_birdseye:
+            l_channel = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)[:,:,0]
+            b_channel = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)[:,:,2]   
+
+            b_thresh_min = 155
+            b_thresh_max = 200
+            b_binary = np.zeros_like(b_channel)
+            b_binary[(b_channel >= b_thresh_min) & (b_channel <= b_thresh_max)] = 1
+            
+            l_thresh_min = 225
+            l_thresh_max = 255
+            l_binary = np.zeros_like(l_channel)
+            l_binary[(l_channel >= l_thresh_min) & (l_channel <= l_thresh_max)] = 1
+
+            combined_binary = np.zeros_like(img[:, :, 0])
+            combined_binary[(l_binary == 1) | (b_binary == 1)] = 1
+
+            combined_binary_lst.append(combined_binary)
         
+        return combined_binary_lst
+
+    def fill_lane(self):
+        binary_warped = self.combined_binary()[0]
+        # Assuming you have created a warped binary image called "binary_warped"
+        # Take a histogram of the bottom half of the image
+        histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
+        # Create an output image to draw on and  visualize the result
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0]/2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set height of windows
+        window_height = np.int(binary_warped.shape[0]/nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Set the width of the windows +/- margin
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 50
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:        
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds] 
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        plt.imshow(out_img)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
     def show_cal_plot(self):
         for orig_img, img in self.cal_imgs_with_corners:
             f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8))
@@ -92,24 +199,20 @@ class Calibration:
             ax1.set_title('Original Image', fontsize=18)
             ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
             ax2.set_title('Warped Image', fontsize=18)
-
-    def show_threshold_plot(image, show=True):
-        img, M = birds_eye(image, display = False)
-
-        for orig_img, img in self.test_imgs_with_birdseye:
-
-            s_channel = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)[:,:,2]
             
-            l_channel = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)[:,:,0]
 
+    def show_threshold_plot(self):
+     
+        for _, img in self.test_imgs_with_birdseye:
+            l_channel = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)[:,:,0]
             b_channel = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)[:,:,2]   
 
-            # Threshold color channel
-            s_thresh_min = 180
-            s_thresh_max = 255
-            s_binary = np.zeros_like(s_channel)
-            s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
-            
+#             # Red channel
+#             thresh = (200, 255)
+#             red = img[:, :, 0]
+#             r_channel = np.zeros_like(red)
+#             r_channel[(red > thresh[0]) & (red <= thresh[1])] = 1
+    
             b_thresh_min = 155
             b_thresh_max = 200
             b_binary = np.zeros_like(b_channel)
@@ -120,32 +223,21 @@ class Calibration:
             l_binary = np.zeros_like(l_channel)
             l_binary[(l_channel >= l_thresh_min) & (l_channel <= l_thresh_max)] = 1
 
-            #color_binary = np.dstack((u_binary, s_binary, l_binary))
-            
-            combined_binary = np.zeros_like(s_binary)
+            combined_binary = np.zeros_like(img[:, :, 0])
             combined_binary[(l_binary == 1) | (b_binary == 1)] = 1
 
             # Plotting thresholded images
-            f, ((ax1, ax2, ax3), (ax4,ax5, ax6)) = plt.subplots(2, 3, sharey='col', sharex='row', figsize=(10,4))
+            f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharey='col', sharex='row', figsize=(10,6))
             f.tight_layout()
             
-            ax1.set_title('Original Image', fontsize=16)
-            ax1.imshow(orig_img, cv2.COLOR_BGR2RGB)
-            
-            ax2.set_title('Warped Image', fontsize=16)
-            ax2.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype('uint8'))
-            
-            ax3.set_title('s binary threshold', fontsize=16)
-            ax3.imshow(s_binary, cmap='gray')
-            
-            ax4.set_title('b binary threshold', fontsize=16)
-            ax4.imshow(b_binary, cmap='gray')
-            
-            ax5.set_title('l binary threshold', fontsize=16)
-            ax5.imshow(l_binary, cmap='gray')
+            ax1.set_title('Warped Image', fontsize=16)
+            ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype('uint8'))
 
-            ax6.set_title('Combined color thresholds', fontsize=16)
-            ax6.imshow(combined_binary, cmap='gray')
-        
-        
-        
+            ax2.set_title('b binary threshold', fontsize=16)
+            ax2.imshow(b_binary, cmap='gray')
+            
+            ax3.set_title('l binary threshold', fontsize=16)
+            ax3.imshow(l_binary, cmap='gray')
+
+            ax4.set_title('Combined color thresholds', fontsize=16)
+            ax4.imshow(combined_binary, cmap='gray')
